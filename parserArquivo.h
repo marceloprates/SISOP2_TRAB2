d70ff249294ddd7d4b1e6c50fab1960a583692dd
Marcelo <marceloorp.gmail.com>
2013-04-18 17:02:04 -0300

    Excluí arquivos

--- parserArquivo.h
+++
@@ -1,8 +0,0 @@
-#ifndef _parserArquivo_h_
-
-#define _parserArquivo_h_ 
-
-int abreArquivoMatriz(const char * nomeArquivo, int * numLinhas, int * numColunas, int *** pMatriz);
-
-
-#endif
--- writerArquivo.h
+++
@@ -1,7 +0,0 @@
-#ifndef _writerArquivo_h_
-
-#define _writerArquivo_h_ 
-
-int escreveArquivoMatriz(char* path, int** matriz, int numLinhas, int numColunas);
-
-#endif
